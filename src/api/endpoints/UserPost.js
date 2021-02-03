d18b9042618bc88edb00ddaa8b5dbce30c578021
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-03 09:04:59 -0300

    first commit

---
+++ src/api/endpoints/UserPost.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const UserPost = () => {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default UserPost;

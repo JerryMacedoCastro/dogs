7adf674fade677b92e83202d15850d0fd21eca0f
Jerry Macedo Castro <leom551@hotmail.com>
2021-03-06 13:39:56 -0300

    create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { ReactComponent as DogFooterIcon } from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <DogFooterIcon />
+      <p>
+        Desenvolvido por: <strong> Jerry Macedo Castro </strong> <br />
+        <a href="mailto:jerry.castro@outlook.com">Contate-me ✉</a> <br />
+        <i>Alguns direitos reservados</i>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;

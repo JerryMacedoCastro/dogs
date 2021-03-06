d7a40e05e72a694a9d7b54d39e3ef5bdddf382d3
Jerry Macedo Castro <leom551@hotmail.com>
2021-03-06 16:42:04 -0300

    add victory and create graphs

--- src/components/User/UserStatisticsGraph.js
+++ src/components/User/UserStatisticsGraph.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import styles from './UserStatisticsGraph.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatisticsGraph = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
+  const [hasPhotos, setHasphotos] = React.useState(false);
 
   React.useEffect(() => {
-    setTotal(
-      data
-        .map(({ acessos }) => Number(acessos))
-        .reduce((accum, curr) => accum + curr)
-    );
-  }, [data]);
+    setHasphotos(data.length !== 0);
+    if (hasPhotos) {
+      const graphData = data.map((item) => {
+        return { x: item.title, y: Number(item.acessos) };
+      });
+      setGraph(graphData);
+      setTotal(
+        data
+          .map(({ acessos }) => Number(acessos))
+          .reduce((accum, curr) => accum + curr)
+      );
+    }
+  }, [data, hasPhotos]);
   return (
     <section className={`${styles.graph} animeLeft`}>
-      <div className={styles.total}>
-        <p>Acessos: {total}</p>
+      <div className={`${styles.total} ${styles.graphItem}`}>
+        <p>{hasPhotos ? `Acessos: ${total}` : 'Nenhuma foto postada ;('}</p>
+      </div>
+
+      <div className={`${styles.graphItem}`}>
+        {hasPhotos && (
+          <VictoryPie
+            data={graph}
+            innerRadius={50}
+            padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+            style={{
+              data: {
+                fillOpacity: 0.9,
+                stroke: '#fff',
+                strokeWidth: 2,
+              },
+              label: { fontSize: 14, fill: '#333' },
+            }}
+          />
+        )}
+      </div>
+      <div className={styles.graphItem}>
+        {hasPhotos && (
+          <VictoryChart>
+            <VictoryBar alignment="start" data={graph} />
+          </VictoryChart>
+        )}
       </div>
     </section>
   );
